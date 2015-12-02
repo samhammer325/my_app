b53fd0c6c40bea13238ebdb0b43eb086e3ef9cd1
Sam Hammer <sam@michaelrobertson.com>
2015-12-02 13:50:21 -0700

    create posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path #redirect to posts if post created
+    else
+      render :new # otherwise redirects to form
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :author, :body)
+  end
+
+end
